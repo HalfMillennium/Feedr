7c68f98916ebcfd328a70dbf9e8bd6d059986a79
HalfMillennium <glc2266@gmail.com>
2018-12-27 18:54:06 -0500

    v0.45
    Integrating settings configurations with custom toolbar w/ overflow menu.

--- app/src/main/java/com/digitalnode/glc22/feedr/MainActivity.java
+++ app/src/main/java/com/digitalnode/glc22/feedr/MainActivity.java
@@ -3,9 +3,27 @@ package com.digitalnode.glc22.feedr;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.digitalnode.glc22.feedr.Sorts.SortByNewest;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.Version;
+import net.dean.jraw.http.NetworkAdapter;
+import net.dean.jraw.http.OkHttpNetworkAdapter;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.models.SubredditSort;
+import net.dean.jraw.models.TimePeriod;
+import net.dean.jraw.oauth.Credentials;
+import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.DefaultPaginator;
+import net.dean.jraw.pagination.Paginator;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,13 +38,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
         Log.d(TAG, "onCreate: Started.");
 
-        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        // You'll want to change this for your specific OAuth2 app
+        Credentials credentials = Credentials.script("<username>", "<password>", "<client ID>", "<client secret>");
 
-       // mViewPager = (ViewPager) findViewById(R.id.container);
-        //setup the pager
-        setupViewPager(mViewPager);
+        // Construct our NetworkAdapter
+        UserAgent userAgent = new UserAgent("bot", "net.dean.jraw.example.script", Version.get(), "thatJavaNerd");
+        NetworkAdapter http = new OkHttpNetworkAdapter(userAgent);
+
+        // Authenticate our client
+        RedditClient reddit = OAuthHelper.automatic(http, credentials);
+
+        // Browse through the top posts of the last month, requesting as much data as possible per request
+        DefaultPaginator<Submission> paginator = reddit.frontPage()
+                .limit(Paginator.RECOMMENDED_MAX_LIMIT)
+                .sorting(SubredditSort.TOP)
+                .timePeriod(TimePeriod.MONTH)
+                .build();
+
+        // Request the first page
+        Listing<Submission> firstPage = paginator.next();
+
+        for (Submission post : firstPage)
+            if (post.getDomain().contains("imgur.com")) {
+                System.out.println(String.format("%s (/r/%s, %s points) - %s",
+                        post.getTitle(), post.getSubreddit(), post.getScore(), post.getUrl()));
+            }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.feed_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.conf_feed:
+                // start activity for configuring the feed
+                return true;
+            case R.id.conf_accounts:
+                // start activity for configuring accounts
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupViewPager(ViewPager viewPager){
